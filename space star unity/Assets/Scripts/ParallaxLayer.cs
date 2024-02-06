6890c96d431c15545e7f6b3aa4f5efd3ca57333e
anyoneforbread <157482990+anyoneforbread@users.noreply.github.com>
2024-02-06 23:40:36 +0800

    continuous background (incomplete)

--- space star unity/Assets/Scripts/ParallaxLayer.cs
+++ space star unity/Assets/Scripts/ParallaxLayer.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class ParallaxLayer : MonoBehaviour
 {
+    private float length, startpos;
+    public GameObject cam;
     public float parallaxFactor;
 
     public void Move(float delta)
@@ -11,6 +14,25 @@ public class ParallaxLayer : MonoBehaviour
         newPos.y -= delta * parallaxFactor;
 
         transform.localPosition = newPos;
+    }
+
+    void Start()
+    {
+        startpos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
+        //UnityEngine.Debug.Log($"length: {length}");
+        //UnityEngine.Debug.Log($"startpos: {startpos}");
+    }
+
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.y * (1-parallaxFactor));
+        transform.position = new Vector3(transform.position.x, startpos, transform.position.z);
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+        
+        UnityEngine.Debug.Log($"temp: {temp}");
+        UnityEngine.Debug.Log($"startpos: {startpos}");
     }
 
 }
