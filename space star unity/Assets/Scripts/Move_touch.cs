7d9685a7c45f30e38c54854e49264b9fffd504ed
keengkai <157497533+keengkai@users.noreply.github.com>
2024-02-06 23:23:04 +0800

    Level selection, main menu
    Level selection is in progress, added some game objects to illustrate the layout, script to scroll through the levels is being developed
    The main menu scene is renamed to level selection because the levels take most of the space
    Another main menu scene is added
    Buttons to go around these scenes are also added (main menu to and from level selection, level selection to solar system, solar system to main menu)

--- space star unity/Assets/Scripts/Move_touch.cs
+++ space star unity/Assets/Scripts/Move_touch.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class move_touch : MonoBehaviour
 {
-    Touch touch;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Touch touch;
+    private Vector2 startPos;
+    private Vector2 direction;
+    private bool validMovement = false;
+    public float mult = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    break;
+
+                case TouchPhase.Moved:
+                    //direction = touch.position - startPos;
+                    //validMovement = (direction.x * direction.y) > 0 ? true : false;
+                    validMovement = (touch.deltaPosition.x * touch.deltaPosition.y) > 0 ? true : false;
+                    break;
+
+                case TouchPhase.Ended:
+                    validMovement = false;
+                    break;
+            }
+            
+            if (validMovement)
+            {
+                transform.position = new Vector3(
+                transform.position.x + touch.deltaPosition.x * mult,
+                transform.position.y + touch.deltaPosition.y * mult,
+                transform.position.z);
+            }
+        }
     }
 }
