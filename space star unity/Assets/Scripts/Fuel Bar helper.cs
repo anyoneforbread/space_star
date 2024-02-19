f1127d22712df9f523077aef76e807518ffad9bd
anyoneforbread <157482990+anyoneforbread@users.noreply.github.com>
2024-02-19 22:25:54 +0800

    sliders now smooth

--- space star unity/Assets/Scripts/Fuel Bar helper.cs
+++ space star unity/Assets/Scripts/Fuel Bar helper.cs
@@ -8,16 +8,27 @@ public class FuelBar : MonoBehaviour
 {
     public Slider fuelSlider;
     public TextMeshProUGUI valueText;
+    [SerializeField] private float slidingTime = 0.1f;
+    private float velocity;
+    private float target;
 
     public void SetSlider(float amount)
     {
-        fuelSlider.value = amount;
+        //fuelSlider.value = amount;
+        target = amount;
         valueText.text = amount.ToString();
     }
 
     public void setSliderMax(float amount)
     {
         fuelSlider.maxValue = amount;
-        SetSlider(amount);
+        fuelSlider.value = amount;
+        target = amount;
+        valueText.text = amount.ToString();
+    }
+
+    void Update()
+    {
+        fuelSlider.value = Mathf.SmoothDamp(fuelSlider.value, target, ref velocity, slidingTime);
     }
 }
--- space star unity/Assets/Scripts/Health Bar helper.cs
+++ space star unity/Assets/Scripts/Health Bar helper.cs
@@ -7,17 +7,29 @@ using TMPro;
 public class HealthBar : MonoBehaviour
 {
     public Slider healthSlider;
-    public TextMeshProUGUI valueText;
+    [SerializeField] private TextMeshProUGUI valueText;
+    [SerializeField] private float slidingTime = 0.1f;
+    private float velocity;
+    private float target;
 
     public void SetSlider(float amount)
     {
-        healthSlider.value = amount;
+        //healthSlider.value = amount;
+        target = amount;
         valueText.text = amount.ToString();
     }
 
     public void setSliderMax(float amount)
     {
         healthSlider.maxValue = amount;
-        SetSlider(amount);
+        healthSlider.value = amount;
+        target = amount;
+        valueText.text = amount.ToString();
     }
+
+    void Update()
+    {
+        healthSlider.value = Mathf.SmoothDamp(healthSlider.value, target, ref velocity, slidingTime);
+    }
+
 }
